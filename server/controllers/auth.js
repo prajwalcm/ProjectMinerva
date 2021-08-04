1d8a8d153fed45eafbf3471e8a4e06c6bb10ab53
manzonm <manzon.m@northeastern.edu>
2021-08-04 10:22:41 -0400

    create user schema

---
+++ server/controllers/auth.js
@@ -0,0 +1,5 @@
+exports.createOrUpdateUser = (req, res) => {
+    res.json({
+        data: "you hit the create-or-update-user API endpoint",
+    });
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/create-or-update-user', (req, res) => {
-    res.json({
-        data: "you reached the create-or-update-user API endpoint"
-    })
-});
+const { createOrUpdateUser } = require('../controllers/auth');
+
+router.get('/create-or-update-user', createOrUpdateUser);
 
 module.exports = router;
