0e11597fa2b06fd765130cdeff602cfc403045a8
manzonm <manzon.m@northeastern.edu>
2021-08-04 11:10:54 -0400

    implement firebase in backend

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -5,6 +5,19 @@ import { Button } from "antd";
 import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+
+const createOrUpdateUser = async (authToken) => {
+  return await axios.post(
+    `${process.env.REACT_APP_API}/create-or-update-user`, 
+    {}, 
+    {
+      headers: {
+        authToken,
+      },
+    }
+  );
+};
 
 const Login = ({ history }) => {
   const [email, setEmail] = useState("gqlreactnode@gmail.com");
@@ -29,14 +42,18 @@ const Login = ({ history }) => {
       const { user } = result;
       const idTokenResult = await user.getIdTokenResult();
 
-      dispatch({
-        type: "LOGGED_IN_USER",
-        payload: {
-          email: user.email,
-          token: idTokenResult.token,
-        },
-      });
-      history.push("/");
+      createOrUpdateUser(idTokenResult.token)
+        .then((res) => console.log("CREATE OR UPDATE RES", res))
+        .catch();
+
+      // dispatch({
+      //   type: "LOGGED_IN_USER",
+      //   payload: {
+      //     email: user.email,
+      //     token: idTokenResult.token,
+      //   },
+      // });
+      // history.push("/");
     } catch (error) {
       console.log(error);
       toast.error(error.message);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const { authCheck } = require('../middlewares/auth');
+
 const { createOrUpdateUser } = require('../controllers/auth');
 
-router.get('/create-or-update-user', createOrUpdateUser);
+router.post('/create-or-update-user', authCheck, createOrUpdateUser);
 
 module.exports = router;
