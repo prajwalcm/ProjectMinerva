52096f2aed52576bcd67d696c4ef28e5951dd600
manzonm <manzon.m@northeastern.edu>
2021-08-04 14:03:26 -0400

    implement user change password form

--- client/src/pages/user/Password.js
+++ client/src/pages/user/Password.js
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserNav from '../../components/nav/UserNav';
+import { auth } from "../../firebase";
+import { toast } from "react-toastify";
 
-const History = () => (
-    <div className="container-fluid">
-        <div className="row">
-            <div className="colmd-2">
-                <UserNav />
+const Password= () => {
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        await auth.currentUser
+            .updatePassword(password)
+            .then(() => {
+                setLoading(false);
+                setPassword("");
+                toast.success("Password updated");
+            })
+            .catch((err) => {
+                setLoading(false);
+                toast.error(err.message);
+            })
+    };
+
+
+    const passwordUpdateForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Your Password</label>
+                <input
+                    type="password" 
+                    onChange={e => setPassword(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter new password"
+                    disabled={loading}
+                    value={password}
+                />
+                <button 
+                    className="btn btn-primary" 
+                    disabled={!password || password.length < 6 || loading}
+                >
+                    Submit
+                </button>
             </div>
-            <div className="col">Change password page</div>
-        </div>
-    </div>
-);
+        </form>
+    );
 
-export default History;
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2">
+                    <UserNav />
+                </div>
+                <div className="col">
+                    {loading ? (
+                        <h4 className="text-danger">Loading...</h4>
+                    ) : (
+                        <h4>Password Update</h4>
+                    )}
+                    {passwordUpdateForm()}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Password;
