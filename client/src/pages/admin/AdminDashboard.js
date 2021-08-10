e5db93df68560eb3ddb617563961060d6e0e2f05
Nissar Ahmed <nissarahmed@Nissars-MacBook-Pro.local>
2021-08-10 04:55:28 -0400

    implemented product slug from router, get product on update page and pre populate category

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -2,16 +2,19 @@ import React from "react";
 import AdminNav from "../../components/nav/AdminNav";
 
 const AdminDashboard = () => {
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-2">
-                    <AdminNav />
-                </div>
-                <div className="col">admin dashbaord page</div>
-            </div>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
         </div>
-    );
+
+        <div className="col">
+          <h4>Admin Dashboard</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
