e9e890acaefbbd09b8ce57b96ddb152a507ed70d
manzonm <manzon.m@northeastern.edu>
2021-08-11 12:50:07 -0400

    display image photos in cart

---
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ModalImage from "react-modal-image";
+import laptop from "../../images/laptop.png";
+
+const ProductCardInCheckout = ({ p }) => {
+  return (
+    <tbody>
+      <tr>
+        <td>
+            <div style={{ width: "200px", height: "auto" }}>
+                {p.images.length ? (
+                    <ModalImage small={p.images[0].url} large={p.images[0].url} />
+                ) : (
+                    <ModalImage small={laptop} large={laptop} />
+                )}
+            </div>
+        </td>
+        <td>{p.title}</td>
+        <td>${p.price}</td>
+        <td>{p.brand}</td>
+        <td>{p.color}</td>
+        <td>{p.count}</td>
+        <td>Shipping Icon</td>
+        <td>Delete Icon</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default ProductCardInCheckout;
