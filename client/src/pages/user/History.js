7a80ed12fbdbe1a292a5b16fdb33dd1ca7ffc148
Nissar Ahmed <nissarahmed@Nissars-MacBook-Pro.local>
2021-08-17 03:05:56 -0400

    implemented user orders in purchase history page and purchase orders in card

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserNav from "../../components/nav/UserNav";
+import { getUserOrders } from "../../functions/user";
+import { useSelector, useDispatch } from "react-redux";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
+import { toast } from "react-toastify";
 
-const History = () => (
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-2">
-        <UserNav />
+const History = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadUserOrders();
+  }, []);
+
+  const loadUserOrders = () =>
+    getUserOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+
+  const showOrderInTable = (order) => <p>each order and it's products</p>;
+
+  const showEachOrders = () =>
+    orders.map((order, i) => (
+      <div key={i} className="m-5 p-3 card">
+        <p>show payment info</p>
+        {showOrderInTable(order)}
+        <div className="row">
+          <div className="col">
+            <p>PDF download</p>
+          </div>
+        </div>
       </div>
-      <div className="col">user history page</div>
+    ));
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <UserNav />
+        </div>
+        <div className="col text-center">
+          <h4>
+            {orders.length > 0 ? "User purchase orders" : "No purchase orders"}
+          </h4>
+          {showEachOrders()}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default History;
