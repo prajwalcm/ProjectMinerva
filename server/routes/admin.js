ca2646a3b380a8084600650237b1fa4d557da543
manzonm <manzon.m@northeastern.edu>
2021-08-17 10:42:50 -0400

    display orders with changable status in admin dashboard

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,7 +5,7 @@ const { authCheck, adminCheck } = require("../middlewares/auth");
 
 const { orders, orderStatus } = require("../controllers/admin");
 
-router.get("admin/orders", authCheck, adminCheck, orders);
+router.get("/admin/orders", authCheck, adminCheck, orders);
 router.put("/admin/order-status", authCheck, adminCheck, orderStatus);
 
 module.exports = router;
